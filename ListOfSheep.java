8b218b6d2e2fa58d227bc7bdd8a2e661f43e90f1
Prashant Singh <prashantlakha@gmail.com>
2023-01-23 15:14:53 -0500

    Fixed the code

--- ListOfSheep.java
+++ ListOfSheep.java
@@ -7,7 +7,7 @@ public class ListOfSheep {
 	
 	public int AddSheep(Sheep sheepToAdd) {
 		// return number of sheep in the Database
-		int numberOfSheep = 0;
+		int numberOfSheep = 2;
 		(this.arrayList).add(sheepToAdd);
 		System.out.println("Added a sheep " + sheepToAdd.SheepName);
 		return numberOfSheep;
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    ListOfSheep obj = new ListOfSheep();
+
+    Sheep sheepObj = new Sheep("Sheepo","White",311,55);
+    obj.AddSheep(sheepObj);
+
+    }
+}
